523d157e0d7c0cb6266a77035e06839e3fb66c55
Ben Lindström <44747@edu.raseko.fi>
2021-10-07 09:41:31 +0300

    otettu ohje kuvat wikiä varten

---
+++ LuoVuokrausAikaNäkymä.sql
@@ -0,0 +1,8 @@
+
+CREATE VIEW dbo.KeskivuokraAikaTuoteryhmittäin
+AS
+SELECT        dbo.tuote.tuoteryhma, AVG(DATEDIFF(day, dbo.vuokraus.alkamispaiva, dbo.vuokraus.paattymispaiva) * dbo.tuote.paivavuokra) AS AikaKeskiMäärin
+FROM            dbo.tuote INNER JOIN
+                         dbo.vuokraus ON dbo.tuote.yksilointikoodi = dbo.vuokraus.yksilointikoodi
+GROUP BY dbo.tuote.tuoteryhma
+GO
